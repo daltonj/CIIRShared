57a47fa11cd29feb8825624048e84f46bd9b47bd
Jeff <jeffdalton104@hotmail.com>
2010-09-10 11:41:07 -0400

    fixed warnings

--- src/edu/umass/ciir/models/JS_KLDivergenceSimilarity.java
+++ src/edu/umass/ciir/models/JS_KLDivergenceSimilarity.java
@@ -29,7 +29,6 @@ public class JS_KLDivergenceSimilarity implements LanguageModelSimilarity {
     	double p1, p2, p_both;
 
     	Collection<TermEntry> vocab1 = lm1.getVocabulary();
-    	Collection<TermEntry> vocab2 = lm2.getVocabulary();
     	double total1 = (double) lm1.getCollectionFrequency();
     	double total2 = (double) lm2.getCollectionFrequency();    	
     	double divergence = 0.0;
